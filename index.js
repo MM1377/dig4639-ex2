c7da8798124663f7b07dbf78b2cfa5e377c52065
MM1377 <77946174+MM1377@users.noreply.github.com>
2021-02-08 14:32:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let quotes = [
+    "Life is like a box of chocolates",
+
+    "Sometimes that is just how it is",
+
+    "Apple a day keeps the docter away",
+
+]
+let index = Math.floor(Math.random() * 3)
+console.log(quotes[index])
+
